063d5759acab0e68cae5913323177b40b6f42a80
vanxxxx <1595891876@qq.com>
2025-01-22 11:18:39 +0800

    fix toggle term

--- lua/plugins/mapping.lua
+++ lua/plugins/mapping.lua
@@ -1,5 +1,4 @@
-local builtin = require('telescope.builtin')
-return{
-vim.keymap.set('n', '<leader><leader>', builtin.find_files, { desc = 'Telescope find files' }),
-vim.keymap.set({'n','v','t'},'<C-_>','<cmd>ToggleTerm direction = float<cr>'),
+local builtin = require "telescope.builtin"
+return {
+  vim.keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Telescope find files" }),
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,18 +1,22 @@
 return {
-{
-"akinsho/toggleterm.nvim", 
-    config = true, 
+  {
+    "akinsho/toggleterm.nvim",
+    config = true,
     cmd = "ToggleTerm",
-    build= ":ToggleTerm",
-    keys = { { "<C-/>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" },{ "<F4>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" } }, 
-    opts = { open_mapping = [[<F4>]], 
-        direction = "float", 
-        shade_filetypes = {}, 
-        hide_numbers = true, 
-        insert_mappings = true, 
-        terminal_mappings = true, 
-        start_in_insert = true, 
-        close_on_exit = true, 
-    }, 
-}, 
-} 
+    build = ":ToggleTerm",
+    keys = {
+      { "<C-/>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" },
+      { "<F4>", "<cmd>ToggleTerm<cr>", desc = "Toggle floating terminal" },
+    },
+    opts = {
+      open_mapping = { [[<C-/>]], [[<F4>]] },
+      direction = "float",
+      shade_filetypes = {},
+      hide_numbers = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      start_in_insert = true,
+      close_on_exit = true,
+    },
+  },
+}

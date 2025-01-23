ab3465575e7676da91aa574b25daa0231276c683
vanxxxx <1595891876@qq.com>
2025-01-23 11:21:17 +0800

    fix pyright

--- init.lua
+++ init.lua
@@ -15,5 +15,6 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
+vim.opt.wrap = true
 require "lazy_setup"
 require "polish"
---
+++ lua/plugins/neovide.lua
@@ -0,0 +1,8 @@
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+
+  vim.g.neovide_scroll_animation_length = 0.1
+end
+return {
+  -- Return any configuration you need
+}
